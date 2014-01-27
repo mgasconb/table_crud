9653f355980b1315e548a6c00f6624175125f3a5
Miguel <Miguel@192.168.1.60>
2014-01-27 18:51:12 +0100

    form and input modificado

--- app/vistas/tabla/form_and_inputs.php
+++ app/vistas/tabla/form_and_inputs.php
@@ -1,27 +1,36 @@
 
 <form method='post' action="?menu=<?php echo $datos['controlador_clase']; ?>&submenu=validar_<?php echo $datos['controlador_metodo']; ?>" >
 	<input id='id' name='id' type='hidden' value='<?php echo \core\Array_Datos::values('id', $datos); ?>' />
-	Categoría: <select id='categoria_nombre' name='categoria_nombre' />
+	Nombre: <select id='nombre' name='nombre' />
 		<?php 
 			if (\core\Distribuidor::get_metodo_invocado() == "form_insertar") {
-				echo "<option >Elige una categría</option>\n";
+				echo "<option >Elige un nombre</option>\n";
 			}
-			foreach ($datos['categorias'] as $categoria) {
-				$selected = (\core\Array_Datos::values('categoria_nombre', $datos) == $categoria['nombre']) ? " selected='selected' " : "";
-				echo "<option $selected>{$categoria['nombre']}</option>\n";
+			foreach ($datos['nombre'] as $nombre) {
+				$selected = (\core\Array_Datos::values('nombre', $datos) == $nombre['nombre']) ? " selected='selected' " : "";
+				echo "<option $selected>{$nombre['nombre']}</option>\n";
 			}
 		?>
 	</select>
-	<?php echo \core\HTML_Tag::span_error('categoria_nombre', $datos); ?>
-	<br />
-	Nombre: <input id='nombre' name='nombre' type='text' size='100'  maxlength='100' value='<?php echo \core\Array_Datos::values('nombre', $datos); ?>'/>
 	<?php echo \core\HTML_Tag::span_error('nombre', $datos); ?>
 	<br />
-	Precio: <input id='precio' name='precio' type='text' size='20'  maxlength='20' value='<?php echo \core\Array_Datos::values('precio', $datos); ?>'/>
-	<?php echo \core\HTML_Tag::span_error('precio', $datos); ?>
+	Apellido paterno: <input id='apellidoPaterno' name='apellidoPaterno' type='text' size='100'  maxlength='100' value='<?php echo \core\Array_Datos::values('apellidoPaterno', $datos); ?>'/>
+	<?php echo \core\HTML_Tag::span_error('apellidoPaterno', $datos); ?>
 	<br />
-	Unidades en Stock: <input id='unidades_stock' name='unidades_stock' type='text' size='20'  maxlength='20' value='<?php echo \core\Array_Datos::values('unidades_stock', $datos); ?>'/>
-	<?php echo \core\HTML_Tag::span_error('unidades_stock', $datos); ?>
+	Apellido materno: <input id='apellidoMaterno' name='apellidoMaterno' type='text' size='20'  maxlength='20' value='<?php echo \core\Array_Datos::values('apellidoMaterno', $datos); ?>'/>
+	<?php echo \core\HTML_Tag::span_error('apellidoMaterno', $datos); ?>
+	<br />
+	Correo: <input id='correo' name='correo' type='text' size='20'  maxlength='20' value='<?php echo \core\Array_Datos::values('correo', $datos); ?>'/>
+	<?php echo \core\HTML_Tag::span_error('correo', $datos); ?>
+	<br />        
+        Nombre de usuario: <input id='username' name='username' type='text' size='20'  maxlength='20' value='<?php echo \core\Array_Datos::values('username', $datos); ?>'/>
+	<?php echo \core\HTML_Tag::span_error('username', $datos); ?>
+	<br />
+        Contraseña: <input id='password' name='password' type='text' size='20'  maxlength='20' value='<?php echo \core\Array_Datos::values('password', $datos); ?>'/>
+	<?php echo \core\HTML_Tag::span_error('password', $datos); ?>
+	<br />
+        Puntuación: <input id='puntuacion' name='puntuacion' type='text' size='20'  maxlength='20' value='<?php echo \core\Array_Datos::values('puntuacion', $datos); ?>'/>
+	<?php echo \core\HTML_Tag::span_error('puntuacion', $datos); ?>
 	<br />
 	<?php echo \core\HTML_Tag::span_error('errores_validacion', $datos); ?>
 	
